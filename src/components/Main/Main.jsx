d78ae4f571365e381c2faf944cf5245d5cd3767c
frandeclich <fdeclich1@gmail.com>
2021-04-20 21:33:13 -0300

    Avances

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,47 +1,65 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Movie from './Movie'
+import Contador from './Contador'
+import Pokemons from './Pokemons'
 
-function Main() {
-    const user={
-        firstName:'Francisco',
-        lastName:'Declich',
-        nickName:'Fran',
-        formatName:function(){
-            return this.firstName+' '+ this.lastName
-        },
-        favouriteMovies:["Alicia en el país de las Maravillas","La momia","El Ángel"]
-    } 
-    const movies=[
-        {
-            title:"Alicia en el país de las Maravillas",
-            rating:8,
-        },
-        {
-            title:"La momia",
-            rating:4,
-        },
-        {
-            title:"El Ángel",
-            rating:6,
-        }
-    ]
-    return (
-        <div>
-            <h2>Las pelis favoritas de {user.nickName}</h2>
-            <ul>
-                {user.favouriteMovies.map(movie=><li>{movie}</li>)}
-                <br/>
-            </ul>
-            <h2>Especificaciones de las películas:</h2>
-            {movies.map((movie,index)=>
-                <Movie
-                    key = {index}
-                    title ={movie.title}
-                    rating ={movie.rating}
-                />)}
-            <h5>{user.formatName()} es un capo.</h5>
-        </div>
-    );
+class Main extends Component{
+    componentDidMount(){
+        console.log('Montado.')
+    }
+    componentDidUpdate(){
+        console.log('Actualizado.')
+    }
+    
+    render(){
+        const user={
+            firstName:'Francisco',
+            lastName:'Declich',
+            nickName:'Fran',
+            formatName:function(){
+                return this.firstName+' '+ this.lastName
+            },
+            favouriteMovies:["Alicia en el país de las Maravillas","La momia","El Ángel"]
+        } 
+        const movies=[
+            {
+                title:"Alicia en el país de las Maravillas",
+                rating:8,
+            },
+            {
+                title:"La momia",
+                rating:4,
+            },
+            {
+                title:"El Ángel",
+                rating:6,
+            }
+        ]
+        return (
+            <div className="main-container">
+                <Pokemons/>
+                <h2>Las pelis favoritas de {user.nickName}</h2>
+                <ul>
+                    {user.favouriteMovies.map(movie=><li>{movie}</li>)}
+                    <br/>
+                </ul>
+                <h2>Especificaciones de las películas:</h2>
+                <ul>
+                {movies.map((movie,index)=>
+                    <Movie
+                        key = {index}
+                        title ={movie.title}
+                        rating ={movie.rating}
+                    />)}
+                </ul>
+                
+                <h5>{user.formatName()} es un capo.</h5>
+                <Contador
+                    valor = {0}
+                />
+            </div>
+        );
+    }
 }
 
 export default Main;
