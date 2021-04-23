cd6a9d5f6a60276033a314361c59d467ca20dbe2
frandeclich <fdeclich1@gmail.com>
2021-04-23 13:33:39 -0300

    introduzco rutas

--- src/App.js
+++ src/App.js
@@ -2,27 +2,47 @@ import React from 'react';
 /* import logo from './logo.svg'; */
 
 /* import './App.css'; */
+import Categories from './components/Header/Categories'
 import Main from './components/Main/Main.jsx'
+import Movie from './components/Main/Movie.jsx'
+import Contador from './components/Main/Contador.jsx'
+import Pokemons from './components/Main/Pokemons.jsx'
+import ReactHookForm from './components/Main/ReactHookForm.jsx'
+import User from './components/Main/User'
+
+import{
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!!!
-        </a>
-      </header> */}
-      <Main />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/contador">
+            <Categories/>
+            <Contador
+              valor = {0}
+            />
+          </Route>
+          <Route path="/main">
+            <Categories/>
+            <Main />
+          </Route>
+          <Route path="/pokemons">
+            <Categories/>
+            <Pokemons/>
+          </Route>
+          <Route path="/usuarios/:id" component={User}/>
+          <Route path="/" exact>
+            <Categories/>
+          </Route>
+        </Switch>
+      </div> 
+    </Router>
   );
 }
 
---
+++ src/components/Header/Categories.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import{
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from 'react-router-dom'
+
+function Categories() {
+    return(
+        <header className="App-header">
+            <nav>
+            <ul>
+                <li>
+                <Link to="/">Home</Link>
+                </li>
+                <li>
+                <Link to="/main">Main</Link>
+                </li>
+                <li>
+                <Link to="/contador">Contador</Link>
+                </li>
+                <li>
+                <Link to="/movies">Movies</Link>
+                </li>
+                <li>
+                <Link to="/pokemons">Pokemons</Link>
+                </li>
+                <li>
+                <Link to="/forms">Forms</Link>
+                </li>
+            </ul>
+            </nav>
+            <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+            Learn React!!!
+            </a>
+        </header>
+    )
+}
+export default Categories
